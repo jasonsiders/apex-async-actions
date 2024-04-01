47cb16e0832e262051a6573dac5bdef18d47b99e
Jason Siders <41714875+jasonsiders@users.noreply.github.com>
2024-03-31 21:11:43 -0500

    feature/related list enhancements (#84)
    * removed lwc-related-list dependency, and bumped logger dependency
    
    * implemented new workspace api features
    
    * added additional columns, allow for column wrapping
    
    * updated install documentation

--- force-app/main/default/lwc/asyncActionRelatedList/asyncActionRelatedList.js
+++ force-app/main/default/lwc/asyncActionRelatedList/asyncActionRelatedList.js
@@ -1,42 +1,172 @@
 import { LightningElement, api, wire } from "lwc";
-import getColumns from "./columns";
+import { EnclosingTabId, IsConsoleNavigation, openSubtab } from "lightning/platformWorkspaceApi";
+import { getObjectInfo } from "lightning/uiObjectInfoApi";
+import { NavigationMixin } from "lightning/navigation";
 import { refreshApex } from "@salesforce/apex";
 import { registerRefreshContainer } from "lightning/refresh";
-import ASYNC_ACTION_OBJECT from "@salesforce/schema/AsyncAction__c";
 import getActions from "@salesforce/apex/AsyncActionRelatedListController.getActions";
+import hasAccess from "@salesforce/apex/AsyncActionRelatedListController.hasAccess";
+import ASYNC_ACTION_OBJECT from "@salesforce/schema/AsyncAction__c";
+import CREATED_DATE_FIELD from "@salesforce/schema/AsyncAction__c.CreatedDate";
+import ERROR_FIELD from "@salesforce/schema/AsyncAction__c.Error__c";
+import ID_FIELD from "@salesforce/schema/AsyncAction__c.Id";
+import NAME_FIELD from "@salesforce/schema/AsyncAction__c.Name";
+import PROCESSOR_FIELD from "@salesforce/schema/AsyncAction__c.ProcessorClass__c";
+import RETRIES_FIELD from "@salesforce/schema/AsyncAction__c.Retries__c";
+import SCHEDULED_FIELD from "@salesforce/schema/AsyncAction__c.Scheduled__c";
+import STATUS_FIELD from "@salesforce/schema/AsyncAction__c.Status__c";
+const MAX_ROWS = 6;
+const URL_FIELD = "ActionUrl";
 const VIEW_ALL_COMPONENT_NAME = "c:asyncActionRelatedPage";
+const COLUMNS = [
+	{
+		fieldName: URL_FIELD,
+		hideDefaultActions: true,
+		includeInRelatedList: true,
+		label: "Async Action",
+		type: "url",
+		typeAttributes: {
+			label: { fieldName: NAME_FIELD?.fieldApiName }
+		}
+	},
+	{
+		fieldName: PROCESSOR_FIELD?.fieldApiName,
+		hideDefaultActions: true,
+		includeInRelatedList: true,
+		label: "Processor Class"
+	},
+	{
+		fieldName: STATUS_FIELD?.fieldApiName,
+		hideDefaultActions: true,
+		includeInRelatedList: true,
+		label: "Status"
+	},
+	{
+		fieldName: ERROR_FIELD?.fieldApiName,
+		hideDefaultActions: true,
+		includeInRelatedList: false,
+		label: "Error"
+	},
+	{
+		fieldName: RETRIES_FIELD?.fieldApiName,
+		hideDefaultActions: true,
+		includeInRelatedList: false,
+		label: "Retries",
+		type: "number"
+	},
+	{
+		fieldName: SCHEDULED_FIELD?.fieldApiName,
+		hideDefaultActions: true,
+		includeInRelatedList: true,
+		isDefaultSort: false,
+		label: "Scheduled",
+		type: "date",
+		typeAttributes: {
+			day: "2-digit",
+			hour: "2-digit",
+			minute: "2-digit",
+			month: "2-digit",
+			year: "2-digit"
+		}
+	},
+	{
+		fieldName: CREATED_DATE_FIELD?.fieldApiName,
+		hideDefaultActions: true,
+		includeInRelatedList: false,
+		label: "Created Date",
+		type: "date",
+		typeAttributes: {
+			day: "2-digit",
+			hour: "2-digit",
+			minute: "2-digit",
+			month: "2-digit",
+			year: "2-digit"
+		}
+	}
+];
 
-export default class AsyncActionRelatedList extends LightningElement {
+export default class AsyncActionRelatedList extends NavigationMixin(LightningElement) {
 	@api recordId;
-	actions = [];
-	actionObject = ASYNC_ACTION_OBJECT?.objectApiName;
-	allColumns = getColumns();
+	_actions = [];
 	cachedQueryResponse;
-	isLoading = true;
+	hasMore = false;
+	_isLoading = true;
 	refreshContainerId;
 
-	get relatedListColumns() {
-		// Display a subset of all columns for the related list page
-		// The related list page will contain the full list of columns
-		const subset = this.allColumns?.filter((column) => {
-			return column?.includeInRelatedList === true;
-		});
-		return subset;
+	get actions() {
+		return this._actions || [];
+	}
+
+	set actions(value) {
+		// Use this property to store records/rows from the parent component to display in the list.
+		// Note: Setter used to enforce the maximum # of rows, defined by the MAX_ROWS property.
+		const numRows = value?.length || 0; 
+		this.hasMore = (numRows > MAX_ROWS); 
+		this._actions = value?.slice(0, MAX_ROWS);
+	}
+
+	get actionObjectName() {
+		return ASYNC_ACTION_OBJECT?.objectApiName;
+	}
+
+	get columns() {
+		return COLUMNS?.filter((column) => column?.includeInRelatedList);
+	}
+
+	get hasActions() {
+		return !!this.actions?.length; 
+	}
+
+	get hasViewAccess() {
+		return this.accessResponse?.data;
+	}
+
+	get header() {
+		// Should display the number of records, unless it exceeds the row limit.
+		// then indicate that the number exceeds the maximum display size
+		// The user should click on the "View All" button in this case
+		const numRows = this.actions?.length || 0;
+		const count = (this.hasMore && numRows > 0) ? `${numRows}+` : numRows; 
+		return `${this.title} (${count})`;
+	}
+
+	get idField() {
+		return ID_FIELD?.fieldApiName;
+	}
+
+	get isLoading() {
+		return this._isLoading || false;
+	}
+
+	set isLoading(value) {
+		// Used to manipulate the related list's spinner from parent components.
+		// Note: Setter used to implement a short timeout on disable,
+		// to make it more obvious to users when a refresh occurs
+		const waitMs = (value) ? 0 : 50; 
+		setTimeout(() => { this._isLoading = value }, waitMs);
+	}
+
+	get title() {
+		return this.objectInfoResponse?.data?.labelPlural || "";
 	}
 
 	get viewAllComponent() {
+		// Defines the component to be opened when "View All" is clicked
+		// Note: We're not sure why, but for some reason, schema imports in c:asyncActionRelatedPage 
+		// NEVER include the namespace of the object/field - even in namespaced environments. 
+		// Get around this by passing the (correctly namespaced) values to the component (see below).
 		return {
 			componentDef: VIEW_ALL_COMPONENT_NAME,
 			attributes: {
-				columns: this.allColumns,
-				objectApiName: this.actionObject,
+				columns: COLUMNS,
+				objectApiName: this.actionObjectName,
 				recordId: this.recordId
-			},
-			tabInfo: {
-				iconName: "custom:custom25",
-				title: "Async Actions"
 			}
 		};
+	}
+
+	get viewAllUrl() {
+		return `/one/one.app#${btoa(JSON.stringify(this.viewAllComponent))}`;
 	}
 
 	connectedCallback() {
@@ -49,8 +179,20 @@ export default class AsyncActionRelatedList extends LightningElement {
 		}
 	}
 
+	@wire(hasAccess)
+	accessResponse;
+
+	@wire(EnclosingTabId)
+	currentTabId;
+
+	@wire(IsConsoleNavigation)
+	isConsole;
+
+	@wire(getObjectInfo, { objectApiName: "$actionObjectName" })
+	objectInfoResponse;
+
 	@wire(getActions, { recordId: "$recordId" })
-	queryActions(response) {
+	queryResponse(response) {
 		this.cachedQueryResponse = response;
 		if (response?.data) {
 			this.mapActions(response?.data)?.then((results) => {
@@ -60,7 +202,24 @@ export default class AsyncActionRelatedList extends LightningElement {
 		}
 	}
 
+	async handleRefresh() {
+		this.isLoading = true;
+		await refreshApex(this.cachedQueryResponse);
+		this.isLoading = false;
+	}
+
+	async handleViewAll(event) {
+		// Navigate to the supplied viewAllComponent.
+		// The navigation method depends on if the current app is a console
+		if (this.isConsole) {
+			this.viewAllInConsoleApp();
+		} else {
+			this.viewAllInStandardApp();
+		}
+	}
+
 	async mapActions(data) {
+		// Add the "ActionUrl" field - hyperlink which is used in the Name column
 		const actions = data?.map((row) => {
 			const ActionUrl = `/${row?.Id}`;
 			return { ...row, ActionUrl };
@@ -68,10 +227,26 @@ export default class AsyncActionRelatedList extends LightningElement {
 		return actions;
 	}
 
-	handleRefresh() {
-		this.isLoading = true;
-		refreshApex(this.cachedQueryResponse).then(() => {
-			this.isLoading = false;
+	async viewAllInConsoleApp() {
+		// When being used in the context of a Lightning Console,
+		// use the Workspace API to open the view all component in a new subtab
+		await openSubtab(this.currentTabId, {
+			focus: true,
+			icon: "custom:custom25",
+			iconAlt: this.title,
+			label: this.title,
+			url: this.viewAllUrl
+		});
+	}
+
+	async viewAllInStandardApp() {
+		// When not used in the context of Lightning Console,
+		// use the NavigationMixin to open the view all component in the same window.
+		this[NavigationMixin.Navigate]({
+			type: "standard__webPage",
+			attributes: {
+				url: this.viewAllUrl
+			}
 		});
 	}
 }
